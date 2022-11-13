2224b3c15604748335809cff9716afab5c14cd13
Himasha Weerasinghe <himashatw@gmail.com>
2022-11-13 15:12:05 +0530

    updated

--- server.js
+++ server.js
@@ -5,10 +5,23 @@ const jwt = require("express-jwt");
 const jwks = require("jwks-rsa");
 const axios = require("axios");
 const jwtAuthz = require("express-jwt-authz");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const MessageModel = require("./models/MessageModel");
+const FileModel = require("./models/FileModel");
 const PORT = process.env.PORT || 8080;
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
+
+const fileFilter = (req, res, cb) => {
+  cb(null, true);
+};
+const upload = multer({
+  fileFilter: fileFilter,
+});
+
 // ADD API ROUTES HERE
 
 const verifyToken = jwt.expressjwt({
@@ -23,7 +36,12 @@ const verifyToken = jwt.expressjwt({
   algorithms: ["RS256"],
 });
 
-const checkPermissions = jwtAuthz(["read:workers"], {
+const checkPermissionsWorker = jwtAuthz(["read:workers"], {
+  customScopeKey: "permissions",
+  customUserKey: "auth",
+});
+
+const checkPermissionsManager = jwtAuthz(["read:managers"], {
   customScopeKey: "permissions",
   customUserKey: "auth",
 });
@@ -51,9 +69,68 @@ app.get("/protected", async (req, res) => {
   res.send("Hello World from protected route");
 });
 
-app.get("/worker-validate", checkPermissions, async (req, res) => {
-  res.send("Hello World from protected route");
+app.post("/worker-validate", checkPermissionsWorker, async (req, res) => {
+  if (!req.auth.permissions.includes("read:workers")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const { message, email } = req.body;
+
+  const hashedMessage = await bcrypt.hash(message, 10);
+
+  // save new message
+  const newMessage = new MessageModel({
+    email,
+    text: hashedMessage,
+  });
+
+  try {
+    await newMessage.save();
+    res.status(200).json({ message: "Message stored successfully" });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 });
+
+app.post(
+  "/manager-validate",
+  checkPermissionsManager,
+  upload.single("file"),
+  async (req, res) => {
+    if (!req.auth.permissions.includes("read:managers")) {
+      console.log(req.auth);
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    console.log(req.file);
+
+    const { email } = req.body;
+
+    // save new message
+    const newFile = new FileModel({
+      email,
+      file: req.file.buffer,
+    });
+
+    try {
+      await newFile.save();
+      res.status(200).json({ message: "Message stored successfully" });
+    } catch (error) {
+      res.status(409).json({ message: error.message });
+    }
+  }
+);
+
+mongoose.connect(process.env.MONGO_URL, (error) => {
+  if (error) throw error;
+});
+
+mongoose.connection.on("connected", () =>
+  console.log("Connected to MongoCluster")
+);
+mongoose.connection.on("error", () =>
+  console.log("MongoDB Connection Unsuccessfull")
+);
 
 app.listen(PORT, () => {
   console.log("Server running on PORT : " + PORT);
